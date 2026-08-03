a9767cec41c115937fa331f2080f4e2ae38ec9f5
RRM1000 <RRM1000@users.noreply.github.com>
2026-08-03 15:01:52 +0100

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -30,6 +31,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-79MKLEGNPH" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-79MKLEGNPH');`}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
